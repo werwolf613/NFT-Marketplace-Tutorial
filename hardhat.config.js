d89d300beb57111eaa9751e43d200ae555cde964
werwolf613 <liuxuehui613@hotmail.com>
2022-08-17 05:27:20 +0000

    ww

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 const fs = require('fs');
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+require('dotenv').config();
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -12,22 +13,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 module.exports = {
-  defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-      chainId: 1337
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d`,
-      //accounts: [process.env.privateKey]
-    },
-    matic: {
-      url: "https://polygon-mainnet.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d",
-      //accounts: [process.env.privateKey]
-    },
     goerli: {
-      url: process.env.REACT_APP_ALCHEMY_API_URL,
-      accounts: [ process.env.REACT_APP_PRIVATE_KEY ]
+      url: process.env.https://eth-goerli.g.alchemy.com/v2/L-N4tE-x_zVumUuokG6BaVSdph4GNcb4,
+      accounts: [process.env.0c6076905b7db6be2e162309034a76592d379d1f55d8b4e0b433d92eef3d8396]
     }
   },
   solidity: {
--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -13,7 +13,7 @@ const sampleData = [
         "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
         "price":"0.03ETH",
         "currentlySelling":"True",
-        "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
+        "address":"0xa613248C7f494eb8B928fFE87903aA88d5cBB503",
     },
     {
         "name": "NFT#2",
@@ -22,7 +22,7 @@ const sampleData = [
         "image":"https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
         "price":"0.03ETH",
         "currentlySelling":"True",
-        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+        "address":"0xa613248C7f494eb8B928fFE87903aA88d5cBB503",
     },
     {
         "name": "NFT#3",
@@ -31,7 +31,7 @@ const sampleData = [
         "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
         "price":"0.03ETH",
         "currentlySelling":"True",
-        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+        "address":"0xa613248C7f494eb8B928fFE87903aA88d5cBB503",
     },
 ];
 const [data, updateData] = useState(sampleData);
